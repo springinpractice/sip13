9a69f13ad0ce6033eb13a005384ca7847d676060
Willie Wheeler <willie.wheeler@gmail.com>
2013-02-06 02:20:51 -0800

    Updated transformer

--- helpdesk/src/main/java/com/springinpractice/ch13/helpdesk/integration/transformer/TicketTransformer.java
+++ helpdesk/src/main/java/com/springinpractice/ch13/helpdesk/integration/transformer/TicketTransformer.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Component;
 
+import com.springinpractice.ch13.cdm.Customer;
 import com.springinpractice.ch13.cdm.Ticket;
 import com.springinpractice.ch13.helpdesk.model.TicketEntity;
 
@@ -15,10 +16,30 @@ public class TicketTransformer {
 	public TicketEntity toEntity(Ticket ticketDto) {
 		TicketEntity ticketEntity = new TicketEntity();
 		ticketEntity.setCategory(ticketCategoryTransformer.toEntity(ticketDto.getCategory()));
-		ticketEntity.setCustomerUsername(ticketDto.getCreatedBy().getUsername());
+		
+		// We do either (1) the username or else (2) the e-mail and full name, but not both.
+		Customer customerDto = ticketDto.getCreatedBy();
+		String username = customerDto.getUsername();
+		if (username != null) {
+			ticketEntity.setCustomerUsername(username);
+		} else {
+			ticketEntity.setCustomerEmail(customerDto.getEmail());
+			ticketEntity.setCustomerFullName(getFullName(customerDto));
+		}
+		
 		ticketEntity.setDateCreated(ticketDto.getDateCreated());
 		ticketEntity.setDescription(ticketDto.getDescription());
 		ticketEntity.setStatus(ticketStatusTransformer.toEntity(ticketDto.getStatus()));
 		return ticketEntity;
+	}
+	
+	private String getFullName(Customer customerDto) {
+		String firstName = customerDto.getFirstName();
+		String lastName = customerDto.getLastName();
+		if (firstName == null) {
+			return (lastName == null ? "[Unknown]" : lastName).trim();
+		} else {
+			return (lastName == null ? firstName : firstName + " " + lastName).trim();
+		}
 	}
 }
